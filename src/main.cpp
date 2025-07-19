b841c6a7bc2bd46c8e4b61f1423c12221a6bb63d
Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2025-07-18 22:31:28 -0400

    feat: descriptive comments

--- src/main.cpp
+++ src/main.cpp
@@ -7,26 +7,35 @@
 
 void log_error(std::string_view);
 
+namespace constants {
+    constexpr int width = 640;
+    constexpr int height = 480;
+};
+
 int main(void) {
+    // load glfw
     if (!glfwInit()) {
         log_error("GLFW failed to initialize");
         return EXIT_FAILURE;
     }
 
-    GLFWwindow* window = glfwCreateWindow(640, 480, "test window", NULL, NULL);
+    // create a window
+    GLFWwindow* window = glfwCreateWindow(
+            constants::width, constants::height, "test window", NULL, NULL);
     if (!window) {
         log_error("GLFW was unable to create a window");
         glfwTerminate();
         return EXIT_FAILURE;
     }
-
     glfwMakeContextCurrent(window);
 
+    // load glad
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        log_error("Failed to initialize GLAD");
+        log_error("GLAD failed to initialize");
         return EXIT_FAILURE;
     }
 
+    // --- MAIN LOOP ---
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
         glfwSwapBuffers(window);
